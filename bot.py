a1dd5cf0e967f1e646552285e1ae9fa0deabfcf0
patelamaan0912-collab <patelamaan0912@gmail.com>
2026-04-29 21:47:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,18 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 import discord
 from discord.ext import commands
 from discord.ui import View, Button
